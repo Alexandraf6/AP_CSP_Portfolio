8b7097f7c126e8e29b5488cbf58e9110237d4e7e
Alexandraf6 <alexandraf69@nycstudents.net>
2024-10-29 09:48:20 -0400

    Create emoji_project.py

---
+++ emoji_project/emoji_project.py
@@ -0,0 +1,33 @@
+# Import the CMU Graphics Package:
+from cmu_graphics import *
+
+
+#Ears
+Circle(100,100,75,fill='cornSilk')
+Circle(300,100,75,fill='cornSilk')
+
+#inner ears
+Oval(200,200,200,200)
+
+#Head
+Circle(200,200,150, fill='cornSilk')
+
+#inner ears
+Oval(250,150,180,150,rotateAngle=150)
+
+#nose
+
+#eyes
+
+#hairs
+
+#snout
+
+#mouth
+
+#stars
+
+
+
+# Run program:
+cmu_graphics.run()
