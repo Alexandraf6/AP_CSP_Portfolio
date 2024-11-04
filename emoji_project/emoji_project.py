93b8d87b0f69e34245e313f9970c29621cf3c7b5
Alexandraf6 <alexandraf69@nycstudents.net>
2024-11-03 19:48:21 -0500

    Update emoji_project.py
    Finished final code

--- emoji_project/emoji_project.py
+++ emoji_project/emoji_project.py
@@ -26,18 +26,34 @@ Circle(200,200,150, fill='cornSilk')
 Rect(102,175,65,20, fill='grey')
 Rect(238,175,65,20, fill='grey')
 
-#hairs
+#Crowns
+Rect(225,100,145,20, rotateAngle=220,fill='khaki')
+Polygon(249,56,295,40,300,100, fill='khaki')
+Polygon(275,75,325,70,325,125, fill='khaki',borderWidth=2)
+Polygon(300,100,360,100,360,150, fill='khaki',borderWidth=2)
+
+#CrownOutline
+Line(249,56,295,40,lineWidth=2,fill=rgb(214, 205, 126))
+Line(295,40,300,75,lineWidth=2,fill=rgb(214, 205, 126))
+Line(300,75,325,70,lineWidth=2,fill=rgb(214, 205, 126))
+Line(325,70,325,100,lineWidth=2,fill=rgb(214, 205, 126))
+Line(325,100,360,100,lineWidth=2,fill=rgb(214, 205, 126))
+Line(360,100,360,150,lineWidth=2,fill=rgb(214, 205, 126))
+
+
 
 #snout
 Oval(200,250,110,90, fill=rgb(235, 226, 206))
 
 #nose
-Star(200,250,30,3,roundness=100)
+Star(200,250,30,3,roundness=100,fill='grey')
 
 #mouth
-Label(),100,100,size='10'
+Label("(",225,275,rotateAngle=295,font='monospace', bold= True, size=100, fill='grey')
+Label("(",170,280,rotateAngle=240,font='monospace', bold= True, size=100, fill='grey')
 
-#stars
+
+
 
 
 
